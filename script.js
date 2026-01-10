c59a6f57c4acde375181f81a0cac88c7aee3036c
Nandani Kumari <nandanijha794@gmail.com>
2026-01-10 06:12:14 +0530

    Add expense form and JS logic to append expenses

--- script.js
+++ script.js
@@ -7,12 +7,13 @@ const pay = document.getElementById("pay-met");
 const date = document.getElementById("date");
 const rcheck = document.getElementById("rec");
 const inter = document.getElementById("time");
+const tbody=document.getElementById("expense-table-body")
 
-button.addEventListener("click", function(e){
-    e.preventDefault();
-    console.log("The page refresd, page did not reloaded");
+// button.addEventListener("click", function(e){
+//     e.preventDefault();
+//     console.log("The page refresd, page did not reloaded");
     
-})
+// })
 
     form.addEventListener("submit", function (e) {
     e.preventDefault();
@@ -21,18 +22,38 @@ button.addEventListener("click", function(e){
     const category = cat.value;
     const description = desc.value;
     const paymentMethod = pay.value;
-    const date = date.value;
-    const isRecurring = rCheck.checked;
+    const date1 = date.value;
+const isRecurring = rcheck.checked;
     const interval = inter.value;
 
     console.log(amount);
     console.log(category);
     console.log(description);
     console.log(paymentMethod);
-    console.log(date);
+    console.log(date1);
     console.log(isRecurring);
     console.log(interval);
+    var row=document.createElement('tr');
+    var c1=document.createElement('td')
+    var c2=document.createElement('td')
+    var c3=document.createElement('td')
+    var c4=document.createElement('td')
+    var c5=document.createElement('td')
+    c2.innerText=category
+    c3.innerText=description
+    c1.innerText=amount;
+    c4.innerText=date1
+    c5.innerText=paymentMethod
+    row.appendChild(c2)
+    row.appendChild(c3)
+    row.appendChild(c1)
+    row.appendChild(c4)
+    row.appendChild(c5)
+    tbody.appendChild(row);
+        form.reset();
 });
+
+
 
 
 
